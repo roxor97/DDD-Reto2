79814dd8418c5383e1bb8097c2e14dbb03efb0f9
Roxor <junyperez97@gmail.com>
2022-01-05 01:41:53 -0500

    se agregaron cambios en el agregado perfil

--- src/main/java/com/sofka/ddd/domain/perfil/PerfilChange.java
+++ src/main/java/com/sofka/ddd/domain/perfil/PerfilChange.java
@@ -1,10 +1,15 @@
 package com.sofka.ddd.domain.perfil;
-
+import java.util.HashSet;
+import java.util.Objects;
 import co.com.sofka.domain.generic.EventChange;
+import com.sofka.ddd.domain.perfil.events.NuevaReferenciaAgregada;
+import com.sofka.ddd.domain.perfil.events.PerfilCreado;
+import com.sofka.ddd.domain.perfil.events.ReferenciaEliminada;
 
 public class PerfilChange extends EventChange {
     public PerfilChange(Perfil perfil){
         apply((PerfilCreado event) -> {
+            perfil.referencias = new HashSet<>();
             perfil.idHojaDeVida = Objects.requireNonNull(event.getIdHojaDeVida());
             perfil.fotoDePerfil = Objects.requireNonNull(event.getFotoDePerfil());
             perfil.infoContacto = Objects.requireNonNull(event.getInfoContacto());
