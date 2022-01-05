a0a7ceb076b090ddb92eddd36b3b377beda85481
Roxor <junyperez97@gmail.com>
2022-01-04 23:50:40 -0500

    primer commit

---
+++ src/test/java/com/sofka/ddd/DddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
